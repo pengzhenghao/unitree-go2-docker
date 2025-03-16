307f24fc645205aff0d2dd14bc158b7c07a6e066
pengzhenghao <pzh@cs.ucla.edu>
2025-03-15 21:06:34 -0700

    Format code

--- tests/test_gi.py
+++ tests/test_gi.py
@@ -1,6 +1,6 @@
 import gi
 
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst, GLib
+from gi.repository import Gst
 
 print("Gstreamer {} is successfully installed.".format(Gst.version_string()))
