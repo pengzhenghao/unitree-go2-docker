0c1520a6956fe932f0da0c9f73cea4431c15592e
pengzhenghao <pzh@cs.ucla.edu>
2025-12-05 21:04:55 -0800

    up

--- go2/docker_internal_setup.sh
+++ go2/docker_internal_setup.sh
@@ -4,6 +4,11 @@ set -e
 # Source the ROS environment
 source /opt/ros/foxy/setup.bash
 
+# Ensure CycloneDDS uses our config
+export CYCLONEDDS_URI=file:///app/unitree_ros2/cyclonedds_ws/src/cyclonedds.xml
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export GO2_NETWORK_INTERFACE=enp118s0
+
 # Print an initialization message
 echo "Docker container initialized successfully. ROS2 environment is now set up."
 
